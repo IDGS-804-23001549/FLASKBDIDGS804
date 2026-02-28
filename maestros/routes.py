6aad507cc7941ca98c9c6f157de2261ce6d71ed6
IDGS-804-23001549 <hernandezurielcam@gmail.com>
2026-02-28 09:46:17 -0600

    maestros completo

--- maestros/routes.py
+++ maestros/routes.py
@@ -1,36 +1,74 @@
 from . import maestros
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 import forms
-from maestros.routes import maestros, maestros
-from models import db
-from models import Alumnosdb, Maestros
+from models import db, Maestros
 
-@maestros.route('/perfil/<nombre>')
-def perfil(nombre):
-    return f"perfil de {nombre}"
-
-@maestros.route("/maestros", methods = ['GET', 'POST'])
 @maestros.route("/index")
+@maestros.route("/")
 def index():
-    create_form = forms.UserForm(request.form)
-    maestros = Maestros.query.all()
-    return render_template("maestros/listadoMaes.html", form=create_form, maestros=maestros)
+    maestros_list = Maestros.query.all()
+    return render_template("maestros/listadoMaes.html", maestros=maestros_list)
 
-@maestros.route('/maestros', methods=['GET', 'POST'])
-def alumnos_view():
+@maestros.route('/crear', methods=['GET', 'POST'])
+def crear():
     create_form = forms.MaestroForm(request.form)
-
     if request.method == "POST" and create_form.validate():
         maes = Maestros(
             nombre=create_form.nombre.data,
-            apellido=create_form.apellido.data,
-            email=create_form.email.data,
-            telefono = create_form.telefono.data
+            apellidos=create_form.apellidos.data,
+            especialidad=create_form.especialidad.data,
+            email=create_form.email.data
         )
-        db.session.add(alum)
+        db.session.add(maes)
         db.session.commit()
-        return redirect(url_for("index"))
-    else:
-        print(create_form.errors)
+        return redirect(url_for("maestros.index"))
+    
+    return render_template("maestros/crearMaestro.html", form=create_form)
 
-    return render_template("Alumnos.html", form=create_form)
+@maestros.route("/detalles", methods=["GET"])
+def detalles():
+    matricula = request.args.get("id") # En tu HTML usaste 'id' en el link
+    maestro = Maestros.query.get_or_404(matricula)
+    return render_template("maestros/detalleMaestro.html", maestro=maestro)
+
+@maestros.route('/modificar', methods=['GET', 'POST'])
+def modificar():
+    matricula = request.args.get('matricula')
+    maes = Maestros.query.get_or_404(matricula)
+    
+    # Si es GET, cargamos el objeto. Si es POST, cargamos lo que envió el usuario
+    if request.method == 'GET':
+        create_form = forms.MaestroForm(obj=maes)
+    else:
+        create_form = forms.MaestroForm(request.form)
+
+    if request.method == 'POST' and create_form.validate():
+        maes.nombre = create_form.nombre.data
+        maes.apellidos = create_form.apellidos.data
+        maes.especialidad = create_form.especialidad.data
+        maes.email = create_form.email.data
+        
+        db.session.commit()
+        return redirect(url_for('maestros.index'))
+    
+    # Si hay errores de validación, imprimirlos para saber qué pasa
+    if create_form.errors:
+        print(create_form.errors)
+        
+    return render_template('maestros/modificarMaestro.html', form=create_form, matricula=matricula)
+
+@maestros.route('/eliminar', methods=['GET', 'POST'])
+def eliminar():
+    matricula = request.args.get('id')
+    maes = Maestros.query.get_or_404(matricula)
+    
+    # Creamos la instancia del formulario
+    create_form = forms.MaestroForm(obj=maes)
+    
+    if request.method == 'POST':
+        db.session.delete(maes)
+        db.session.commit()
+        return redirect(url_for('maestros.index'))
+    
+    # IMPORTANTE: Pasar 'form' al template
+    return render_template('maestros/eliminarMaestro.html', maestro=maes, form=create_form)
